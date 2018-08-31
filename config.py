825c394822ea0dd4ada3d91254988daf1a351554
EugeneZnm <eugenenzioki@gmail.com>
2018-08-31 17:56:33 +0300

    base url edit

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ import os
 
 
 class Config:
-    SOURCES_API_BASE_URL = "https://newsapi.org/v2/sources?language=en&category={}&apiKey={}"
+    SOURCES_API_BASE_URL = "https://newsapi.org/v2/sources?category={}&apiKey={}"
     SOURCES_API_KEY = os.environ.get('SOURCE_API_KEY')
 
 
