33c7314a60167de8d263dc06f6e8189dc998f36f
EugeneZnm <eugenenzioki@gmail.com>
2018-08-31 15:27:18 +0300

    application factory function

--- app/_init_.py
+++ app/_init_.py
@@ -4,3 +4,12 @@ from flask_bootstrap import Bootstrap
 # import config options
 from config import config_options
 
+bootstrap = Bootstrap()
+
+def create_app(config_name):
+    """
+    creating application factory function
+    :param config_name:
+    :return:
+    """
+    app = Flask(__name__)
