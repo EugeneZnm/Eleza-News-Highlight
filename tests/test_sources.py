fb6543f00b7502a7d76394ff510a06cdb274bf24
EugeneZnm <eugenenzioki@gmail.com>
2018-09-02 23:27:43 +0300

    test to check instance variable of class sources

--- tests/test_sources.py
+++ tests/test_sources.py
@@ -2,7 +2,7 @@
 import unittest
 
 
-#import class Sources
+# import class Sources
 from app.models import Sources
 
 
@@ -11,3 +11,20 @@ class SourcesTest(unittest.TestCase):
     Test instances of test class
 
     """
+    def setUp(self):
+        """
+        setup method to run before tests
+        :return:
+        """
+        self.new_source = Sources("abc-news", "ABC News", "Your trusted source for breaking news, analysis, exclusive interviews, headlines, and videos at ABCNews.com.", "http://abcnews.go.com",
+                                  "general","en", "us")
+
+    def test_to_check_instance_variables(self):
+        self.assertEquals(self.new_source.id, 'abc-news')
+        self.assertEquals(self.new_source.name, 'ABC News')
+        self.assertEquals(self.new_source.description, 'Your trusted source for breaking news, analysis, exclusive interviews, headlines, and videos at ABCNews.com.')
+        self.assertEquals(self.new_source.url, 'http://abcnews.go.com')
+        self.assertEquals(self.new_source.url, 'general')
+        self.assertEquals(self.new_source.country, 'us')
+        self.assertEquals(self.new_source.language, 'en')
+
