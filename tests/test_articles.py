e702cc17c67463e3cc020fc5182f625451ae4430
EugeneZnm <eugenenzioki@gmail.com>
2018-09-02 23:30:09 +0300

    parent class to test articles variables

---
+++ tests/test_articles.py
@@ -0,0 +1,11 @@
+# import articles
+import unittest
+
+# import class Articles
+from app.models import Articles
+
+class ArticlesTest(unittest.TestCase):
+    """
+    Parent class to test articles variables
+
+    """
