cc1892d02b34c12919ad3e7ee31320762b9a4e1a
EugeneZnm <eugenenzioki@gmail.com>
2018-09-02 23:48:48 +0300

    checking for instance variable in Articles test class

--- tests/test_articles.py
+++ tests/test_articles.py
@@ -23,3 +23,15 @@ class ArticlesTest(unittest.TestCase):
                                     "/story?id=57558628",
                                     "https://s.abcnews.com/images/US/police-security-gty-jt-180902_hpMain_16x9_992.jpg",
                                     "2018-09-02T19:57:44Z")
+
+    def test_to_check_instance_variables(self):
+        self.assertEquals(self.new_article.id, "abc-news")
+        self.assertEquals(self.new_article.author, "Luke Barr")
+        self.assertEquals(self.new_article.title, "For safety on campuses, law enforcement increasingly turns to apps")
+        self.assertEquals(self.new_article.description,
+                          "The apps allow students to communicate more effectively with safety officers.")
+        self.assertEquals(self.new_article.url, "https://abcnews.go.com/US/safety-campuses-law-enforcement"
+                                                "-increasingly-turns-apps/story?id=57558628")
+        self.assertEquals(self.new_article.urlToImage, "https://s.abcnews.com/images/US/police-security-gty-jt"
+                                                       "-180902_hpMain_16x9_992.jpg")
+        self.assertEquals(self.new_article.publishedAt, "2018-09-02T19:57:44Z")
