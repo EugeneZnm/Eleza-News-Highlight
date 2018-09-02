b08465dac9e353d41c8601bcda97901ceb8b4368
EugeneZnm <eugenenzioki@gmail.com>
2018-09-02 22:00:12 +0300

    creation of function rendering error page

--- app/main/errors.py
+++ app/main/errors.py
@@ -5,3 +5,10 @@ from flask import render_template
 from . import main
 
 
+@main.app_errorhandler(404)
+def four_four(error):
+    """
+    Fuction rendering a 404 page
+    :param error:
+    :return:
+    """
